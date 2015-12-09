d679722e018077c66f3de129425d52b9ca5e189f
Casey Jones <jonescaseyb@gmail.com>
2015-12-09 17:50:19 +0000

    Problem 6

---
+++ 6/main.swift
@@ -0,0 +1,20 @@
+import Glibc
+
+// Sum of Squares
+
+var sum_sq = 0
+for i in 1...100 {
+	sum_sq += Int(pow(Double(i), 2))
+}
+
+// Square of Sums
+
+var sq_sum = 0
+for i in 1...100 {
+	sq_sum += i
+}
+sq_sum = Int(pow(Double(sq_sum), 2))
+
+var diff = sq_sum - sum_sq
+print(diff)
+
